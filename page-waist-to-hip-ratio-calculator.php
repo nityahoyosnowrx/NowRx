6d527e9241a992e52c5bff74b396f88a4c48669d
nityahoyosnowrx <nitya.hoyos@nowrx.com>
2022-08-26 14:56:17 -0700

    calculator

--- page-waist-to-hip-ratio-calculator.php
+++ page-waist-to-hip-ratio-calculator.php
@@ -9,19 +9,25 @@
         <header>
             <h1>Waist to Hip Ratio Calculator</h1>
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium aliquid nostrum quae odit illum quos, necessitatibutate. Doloribus.
+                Input your measurements into the waist to hip ratio calculator below to determine your risk of cardiovascular and metabolic disease.
             </p>
         </header>
+        <div class="alert_box">
+            <div class="float">
+                Please remember to set your sex, waist circumference and hip circumference.
+            </div>
+        </div>
         <span class="input_field">
+
             <div class="label">Select the sex you were assigned at birth*</div>
 
             <div class="input-container">
                 <div class="input">
-                    <input type="radio" id="male" class="radio-btn" name="sex" value="male">
+                    <input type="radio" required id="male" class="radio-btn" name="sex" value="male">
                     <label class="radio-btn" for="male"> </label>
                     <span class="false-label">Male</span>
 
-                    <input type="radio" id="female" class="radio-btn" name="sex" value="female">
+                    <input type="radio" required id="female" class="radio-btn" name="sex" value="female">
                     <label class="radio-btn" for="female"> </label>
                     <span class="false-label">Female</span>
                 </div>
@@ -32,7 +38,7 @@
             <div class="label">Enter waist circumference</div>
             <div class="input-container border-input">
                 <div class="input">
-                    <input type="number" value="" class="field input-waist">
+                    <input type="number" required value="" class="field input-waist">
                 </div>
                 <div class="unit">
                     <select name="" id="unit-waist">
@@ -47,7 +53,7 @@
             <div class="label">Enter hip circumference</div>
             <div class="input-container border-input">
                 <div class="input">
-                    <input type="number" value="" class="field input-hip">
+                    <input type="number" required value="" class="field input-hip">
                 </div>
                 <div class="unit">
                     <select name="" id="unit-hip">
@@ -84,12 +90,28 @@
                     <h2 class="title">Results</h2>
                 </header>
                 <div class="input_result input_field">
-                    <div class="label">Your waist-hip ratio</div>
+                    <div class="label">Your Waist to Hip Ratio:</div>
                     <div class="input-container results-container">
-                        <input aria-label="Your waist-hip ratio" autocomplete="off" readonly class="input" placeholder="" type="number" value="">
+                        <input aria-label="Your waist-hip ratio" autocomplete="off" size='5' readonly class="input" placeholder="" type="number" value="">
                     </div>
+                    <div class="risk value"></div>
                 </div>
-                <table id="whresults">
+                <div class="risk_results">
+                    <div class="risk_amount r_low">
+                        Your waist to hip ratio of <span class="risk number"></span> puts you in the <b>low-risk</b> category for developing metabolic complications such as heart attack, stroke, and Type 2 Diabetes. To learn more about the research behind waist to hip ratio, please visit the World Health Organization’s website at <a href="https://www.who.int/publications/i/item/9789241501491">https://www.who.int/publications/i/item/9789241501491</a>.
+                    </div>
+                    <div class="risk_amount r_mid">
+                        Your waist to hip ratio of <span class="risk number"></span> puts you in the <b>moderate-risk</b> category for developing metabolic complications such as heart attack, stroke, and Type 2 Diabetes. To learn more about the research behind waist to hip ratio, please visit the World Health Organization’s website at <a href="https://www.who.int/publications/i/item/9789241501491">https://www.who.int/publications/i/item/9789241501491</a>.
+                    </div>
+                    <div class="risk_amount r_high">
+                        Your waist to hip ratio of <span class="risk number"></span> puts you in the <b>high-risk</b> category for developing metabolic complications such as heart attack, stroke, and Type 2 Diabetes. To learn more about the research behind waist to hip ratio, please visit the World Health Organization’s website at <a href="https://www.who.int/publications/i/item/9789241501491">https://www.who.int/publications/i/item/9789241501491</a>.
+                    </div>
+
+                    <span class="disclaimer">
+                        *This assessment is being provided solely for informational purposes and should never take the place of advice from a qualified health professional.
+                    </span>
+                </div>
+                <!-- <table id="whresults">
                     <tbody>
                         <tr>
                             <th class="female">Female</th>
@@ -104,15 +126,15 @@
                         <tr class="tr-moderate">
                             <td class="female">0.81 to 0.84</td>
                             <td class="male">0.96 to 1.0</td>
-                            <td class="whrOrange">Moderate risk</td>
+                            <td class="whrOrange">Moderate-risk</td>
                         </tr>
                         <tr class="tr-high">
                             <td class="female">0.85 or higher</td>
                             <td class="male">1.0 or higher</td>
-                            <td class="whrRed">High risk</td>
+                            <td class="whrRed">High-risk</td>
                         </tr>
                     </tbody>
-                </table>
+                </table> -->
             </div>
         </div>
 
@@ -121,7 +143,7 @@
 
 </section>
 
-<!-- <div class="block-subscribe">
+<div class="block-subscribe">
     <div class="subscribe-blockitem">
         <header class="title">
             <h2 class="title">Subscribe to Healthcare in 5</h2>
@@ -135,7 +157,7 @@
             </div>
         </header>
     </div>
-</div> -->
+</div>
 
 <article>
 
@@ -240,41 +262,58 @@
             }
         });
 
-        $(' button.calculate-btn').on('click', function() {
+        $('.calculate-btn').on('click', function() {
             let valH = $(inputHip).val();
             let valW = $(inputWaist).val();
-            calculateWH(valW, valH)
-            $('.results-block').addClass('active');
+            let radI = $('.radio-btn:checked').val();
+
+            if (valH && valW && radI) {
+                calculateWH(valW, valH)
+                $('.results-block').addClass('active');
+
+                if ($('.alert_box').hasClass('active')) {
+                    $('.alert_box').removeClass('active')
+                }
+                $('.info-block').hide();
+            } else {
+                $('.alert_box').addClass('active');
+            }
+
+
         });
 
-        function convertCMToMM(value){
+
+        function convertCMToMM(value) {
             value = value * 10;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
         }
-        function convertCMToIN(value){
+
+        function convertCMToIN(value) {
             value = value / 2.54;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
         }
 
-        function convertINToMM(value){
+        function convertINToMM(value) {
             value = value * 25.4;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
         }
-        function convertINToCM(value){
+
+        function convertINToCM(value) {
             value = value * 2.54;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
         }
 
-        function convertMMToCM(value){
+        function convertMMToCM(value) {
             value = value / 10;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
         }
-        function convertMMToIN(value){
+
+        function convertMMToIN(value) {
             value = value / 25.4;
             value = Math.round((value + Number.EPSILON) * 100) / 100;
             return value;
@@ -282,23 +321,23 @@
 
         let currentUnit = "cm";
         let inputValWaist = $('.input-waist').val();
-            let inputValHip = $('.input-hip').val();
+        let inputValHip = $('.input-hip').val();
 
-        $('.input-waist, .input-hip').on('change',function(){
-             inputValWaist = $('.input-waist').val();
-             inputValHip = $('.input-hip').val();
+        $('.input-waist, .input-hip').on('change', function() {
+            inputValWaist = $('.input-waist').val();
+            inputValHip = $('.input-hip').val();
         });
 
-        $('#unit-hip, #unit-waist').on('change',function(){
+        $('#unit-hip, #unit-waist').on('change', function() {
             let val = $(this).val();
 
             inputValWaist = $('.input-waist').val();
-             inputValHip = $('.input-hip').val();
+            inputValHip = $('.input-hip').val();
 
             let hField = $('.input-hip');
             let wField = $('.input-waist');
 
-            let cmW =  inputValWaist;
+            let cmW = inputValWaist;
             let cmH = inputValHip;
 
             let wCon = inputValWaist;
@@ -307,13 +346,13 @@
             // let currentUnit = $(this).val();
 
             // selected value
-            if(val == 'in'){
+            if (val == 'in') {
                 $("#unit-hip, #unit-waist").val('in');
                 // if cm
-                if(currentUnit == "cm"){
-                     wCon =  convertCMToIN(inputValWaist);
-                     hCOn = convertCMToIN(inputValHip);
-                    console.log(inputValWaist,wCon)
+                if (currentUnit == "cm") {
+                    wCon = convertCMToIN(inputValWaist);
+                    hCOn = convertCMToIN(inputValHip);
+                    console.log(inputValWaist, wCon)
                     wField.val(wCon);
                     hField.val(hCOn);
 
@@ -322,9 +361,9 @@
 
 
                 // if mm
-                if(currentUnit == "mm"){
-                     wCon =  convertMMToIN(inputValWaist);
-                     hCOn = convertMMToIN(inputValHip);
+                if (currentUnit == "mm") {
+                    wCon = convertMMToIN(inputValWaist);
+                    hCOn = convertMMToIN(inputValHip);
 
                     wField.val(wCon);
                     hField.val(hCOn);
@@ -334,26 +373,26 @@
             }
 
             // selected value
-            if(val == 'cm'){
+            if (val == 'cm') {
                 $("#unit-hip, #unit-waist").val('cm');
                 // if MM
-                if(currentUnit == "in"){
-                     wCon =  convertINToCM(inputValWaist);
-                     hCOn = convertINToCM(inputValHip);
+                if (currentUnit == "in") {
+                    wCon = convertINToCM(inputValWaist);
+                    hCOn = convertINToCM(inputValHip);
 
                     wField.val(wCon);
                     hField.val(hCOn);
 
-                    console.log(inputValWaist,wCon)
+                    console.log(inputValWaist, wCon)
 
                     currentUnit = "cm";
                 }
 
 
                 // if IN
-                if(currentUnit == "mm"){
-                     wCon =  convertMMToCM(inputValWaist);
-                     hCOn = convertMMToCM(inputValHip);
+                if (currentUnit == "mm") {
+                    wCon = convertMMToCM(inputValWaist);
+                    hCOn = convertMMToCM(inputValHip);
 
                     wField.val(wCon);
                     hField.val(hCOn);
@@ -362,12 +401,12 @@
                 }
             }
             // selected value
-            if(val == 'mm'){
+            if (val == 'mm') {
                 $("#unit-hip, #unit-waist").val('mm');
                 // if CM
-                if(currentUnit == "cm"){
-                     wCon =  convertCMToMM(inputValWaist);
-                     hCOn = convertCMToMM(inputValHip);
+                if (currentUnit == "cm") {
+                    wCon = convertCMToMM(inputValWaist);
+                    hCOn = convertCMToMM(inputValHip);
 
                     wField.val(wCon);
                     hField.val(hCOn);
@@ -376,9 +415,9 @@
 
 
                 // if IN
-                if(currentUnit == "in"){
-                     wCon =  convertINToMM(inputValWaist);
-                     hCOn = convertINToMM(inputValHip);
+                if (currentUnit == "in") {
+                    wCon = convertINToMM(inputValWaist);
+                    hCOn = convertINToMM(inputValHip);
 
                     wField.val(wCon);
                     hField.val(hCOn);
@@ -408,7 +447,7 @@
 
             mainResult = Math.round((mainResult + Number.EPSILON) * 100) / 100
 
-            console.log(mainResult)
+            // console.log(mainResult)
 
             if ($('.radio-btn:checked').val() == 'female') {
                 $('td.male,th.male').hide();
@@ -417,29 +456,77 @@
                 $('td.female,th.female').hide();
             }
 
-            if (mainResult <= .93) {
-                $('.tr-low').addClass('m-low');
-            } else if (mainResult >= .94 && mainResult <= 0.95) {
-                $('.tr-low').addClass('m-low');
-            } else if (mainResult >= .96 && mainResult <= 1) {
-                $('.tr-moderate').addClass('m-low');
-            } else if (mainResult >= 1 && mainResult <= 10) {
-                $('.tr-high').addClass('m-high');
+            let riskValue = "";
+
+            $('.risk').html('');
+
+            $('.risk_amount').removeClass('result-active');
+
+            if ($('.radio-btn:checked').val() == 'male') {
+                if (mainResult <= .1) {
+
+                    $('.tr-low').addClass('m-low');
+                    $('.r_low').addClass('result-active');
+                    riskValue = "low-risk";
+
+                } else if (mainResult >= .8 && mainResult <= 0.84) {
+
+                    $('.tr-low').addClass('m-low');
+                    $('.r_low').addClass('result-active');
+                    riskValue = "low-risk";
+
+                } else if (mainResult >= .85 && mainResult <= .89) {
+
+                    $('.tr-moderate').addClass('m-low');
+                    $('.r_mid').addClass('result-active');
+                    riskValue = "moderate-risk";
+
+                } else if (mainResult >= .9 && mainResult <= 100) {
+
+                    $('.tr-high').addClass('m-high');
+                    $('.r_high').addClass('result-active');
+                    riskValue = "high-risk";
+
+                }
             }
 
-            if (mainResult <= .8) {
-                $('.tr-low').addClass('f-low');
-            } else if (mainResult >= .81 && mainResult <= 0.84) {
-                $('.tr-low').addClass('f-low');
-            } else if (mainResult >= .81 && mainResult <= .84) {
-                $('.tr-moderate').addClass('f-low');
-            } else if (mainResult >= 0.85 && mainResult <= 10) {
-                $('.tr-high').addClass('f-high');
+            if ($('.radio-btn:checked').val() == 'female') {
+                if (mainResult <= .7) {
+
+                    $('.tr-low').addClass('f-low');
+                    $('.r_low').addClass('result-active');
+                    riskValue = "low-risk";
+
+                } else if (mainResult >= .71 && mainResult <= 0.79) {
+
+                    $('.tr-low').addClass('f-low');
+                    $('.r_low').addClass('result-active');
+                    riskValue = "low-risk";
+
+                } else if (mainResult >= .8 && mainResult <= .84) {
+
+                    $('.tr-moderate').addClass('f-low');
+                    $('.r_mid').addClass('result-active');
+                    riskValue = "moderate-risk";
+
+                } else if (mainResult >= 0.85 && mainResult <= 100) {
+
+                    $('.tr-high').addClass('f-high');
+                    $('.r_high').addClass('result-active');
+                    riskValue = "high-risk";
+
+                }
             }
 
+
+
+            $('.risk.value').append(riskValue);
+            $('.risk.number').append(mainResult);
             $('.input_result .input').val(mainResult);
 
-            document.getElementById("results").scrollIntoView({behavior: "smooth"});
+            document.getElementById("results").scrollIntoView({
+                behavior: "smooth"
+            });
         }
 
     });
